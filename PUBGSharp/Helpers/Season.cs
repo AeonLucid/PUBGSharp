0c0595061f0268a1ddf1a00b03845a177b71a1f0
AeonLucid <aeonlucid@outlook.com>
2017-08-03 00:37:32 +0200

    Add season 3.

--- PUBGSharp/Helpers/Season.cs
+++ PUBGSharp/Helpers/Season.cs
@@ -4,5 +4,8 @@
     {
         public const string Season1 = "2017-pre1";
         public const string Season2 = "2017-pre2";
+        public const string Season3 = "2017-pre3";
+
+        public const string CurrentSeason = Season3;
     }
 }
