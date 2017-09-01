35355391f26d7a52cd327a802f7c2ebcb375d9bb
AeonLucid <aeonlucid@outlook.com>
2017-09-02 01:05:46 +0200

    Update to new season.

--- PUBGSharp/Helpers/Season.cs
+++ PUBGSharp/Helpers/Season.cs
@@ -5,7 +5,8 @@
         public const string Season1 = "2017-pre1";
         public const string Season2 = "2017-pre2";
         public const string Season3 = "2017-pre3";
+        public const string Season4 = "2017-pre4";
 
-        public const string CurrentSeason = Season3;
+        public const string CurrentSeason = Season4;
     }
 }
